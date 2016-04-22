8e2b8467b44343b5b8cfc2a30f3bb7cb39379000
Dax <gaodacheng@gaodacheng.com>
2016-04-22 21:19:36 +0800

    get all school in a very long list (array) by cities

--- jxedt/get_school.php
+++ jxedt/get_school.php
@@ -1,16 +1,17 @@
 <?php
-include_once '../include/config.php';
-include_once '../include/functions.php';
 /**
  * get school data
  * @author  GDax
  * @date    2016-04-22
  */
 
-/*
-    schools in one city api : http://m.jxedt.com/jiaxiao/sh/pn1
-    school detail api : http://api.jxedt.com/detail/2417/?type=jx
-**/
+include_once '../include/config.php';
+include_once '../include/functions.php';
+
+/**
+ * schools in one city api : http://m.jxedt.com/jiaxiao/sh/pn1
+ * school detail api : http://api.jxedt.com/detail/2417/?type=jx
+ */
 
 // 中国四个直辖市分别为 : 北京 上海 天津 重庆
 $cities = array('bj', 'sh', 'tj', 'cq');
@@ -20,15 +21,41 @@ $school_url = 'http://api.jxedt.com/detail/';
 $db = new PDO('mysql:host=localhost;dbname=xihaxueche', USER, PASS);
 !is_object($db) && exit();
 
-get_school_by_city('bj');
-exit();
-foreach ( $cities as $city ) {
-    $school_list = get_school_by_city($city);
-    var_dump($school_list);
-}
+/**
+ * main part of the crawler
+ * [1] get a very long school list (array)
+ */
+
+$school_list = combine_school_list($cities);
+var_dump(count($school_list));
+
+// Execution of the crawler End
+
+
+/**
+ * Function Definition
+ * [1] combine_school_list
+ * [2] get_school_by_city
+ */
+
+function combine_school_list ( $cities ) {
+    $school_list = array();
+    if ( empty($cities) ) {
+        return false;
+    }
+    foreach ( $cities as $city ) {
+        $school_list_of_one_city = get_school_by_city($city);
+        /*
+        foreach( $school_list_of_one_city as $school) {
+            $school_list[] = $school;
+        }
+        */
+        $school_list = array_merge($school_list, $school_list_of_one_city);
+    }
+    return $school_list;
+} /* combine school list END */
 
 function get_school_by_city( $city ) {
-    static $count = 1;
     $school_list = array();
     $city_url = 'http://m.jxedt.com/jiaxiao/';
     $url = "$city_url$city/pn1";
@@ -37,19 +64,19 @@ function get_school_by_city( $city ) {
         $school_info = file_get_contents($url);
         $school_info = json_decode($school_info, true);
         if ( empty($school_info) ) {
-            break;
+            break; // jump out of the FOR-loop until we can not get the school info
         }
         foreach ( $school_info as $school ) {
-            echo "get one: " . $count . " " . $school['title'] . "\n";
-            $count++;
-            $school_list[] = $school_info;
+            if ( empty($school['title']) ) {
+                continue; // if the school does not have the TITLE, delete it
+            }
+            $school_list[] = $school;
         }
     }
-
     return $school_list;
-}
+} /* get school by one city END */
 
 
 $db = null;
 
-// ^_^ you are reach the end of crawler
+// ^_^ you are neach the end of crawler
