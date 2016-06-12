f578de0677de30ebf4e52978b50b65adb6e6eff8
Dax <me@daxg.org>
2016-06-12 21:26:34 +0800

    最新驾考题库抓取

---
+++ ybjk.com/get-chapter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# get-chapter.py
+
+# refer: 
+# [1] http://www.ybjk.com/kms-zjlx-xc/
+# [2] http://www.ybjk.com/lianxiti-xc.js?r=t287hxc5um&act=kms-sxlx&cx=xc&zid=1511
+# [3] http://jsc.mnks.cn/ybjk/js/lianxi2013_v3.js?t=201512.js
+
+import os
+import sys
+import requests
+from bs4 import BeautifulSoup
+
+reload(sys)
+sys.setdefaultencoding('utf8')
+
+# e.g. http://www.ybjk.com/kms-zjlx-xc/
+baseurl = 'http://www.ybjk.com/'
+
+# 科目一, 科目四
+lesson = ['kmy', 'kms']
+
+# 小车, 摩托车, 货车, 客车
+car = ['xc', 'hc', 'kc', 'mtc']
+
+# 伪造浏览器头信息
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.109 Safari/537.36'}
+
+def urlFact(car, lesson, base = 'http://www.ybjk.com/'):
+    url = base + lesson + '-zjlx-' + car + '/'
+    return url
+
+def urlRequest(url, headers = headers):
+    r = requests.get(url, headers = headers)
+    if not r.status_code == 200:
+        return False
+    soup = BeautifulSoup(r.content, 'lxml')
+    results = soup.findAll('script')
+    for i in results:
+        if 'html' in i.getText():
+             i.getText().split('"')[1][4:]
+
+def main():
+    for c in car:
+        for l in lesson:
+            url = urlFact(car = c, lesson = l)
+            chapter_list = urlRequest(url = url)
+            print chapter_list
+            #for chap in chapter_list:
+                #print c+':'+l+':'+chap
+
+# start
+if __name__ == '__main__':
+    main()
